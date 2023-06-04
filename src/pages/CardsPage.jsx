4b6ed95487e397e9b517b4bece17f738c320e0b5
M.Mostova <mmainfo15@gmail.com>
2023-06-04 14:13:44 +0300

    craeted app

--- src/pages/CardsPage.jsx
+++ src/pages/CardsPage.jsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from "react";
-import { fetchUsers } from "../services/users-api";
-import Loader from "../components/Loader";
-import CardsList from "../components/CardsList";
-import Button from "../components/Button";
-import ErrorMessage from "../components/ErrorMessage";
+import { useEffect, useState } from 'react';
+import { fetchUsers } from '../services/users-api';
+import Loader from '../components/Loader';
+import CardsList from '../components/CardsList';
+import Button from '../components/Button';
+import ErrorMessage from '../components/ErrorMessage';
 
 const CardsPage = () => {
   const [page, setPage] = useState(1);
   const [users, setUsers] = useState([]);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [isLastPage, setIsLastPage] = useState(false);
 
   useEffect(() => {
+    const getUsers = async currentPage => {
+      setIsLoading(true);
+      const limit = 6;
+      try {
+        const result = await fetchUsers(currentPage, limit);
+        setUsers(prevUsers =>
+          page === 1 ? result : [...prevUsers, ...result]
+        );
+        setIsLastPage(result.length < 6);
+      } catch (error) {
+        setError(error);
+        console.error('Error updating follower count:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
     getUsers(page);
   }, [page]);
 
-  const getUsers = async (currentPage) => {
-    setIsLoading(true);
-    const limit = 6;
-    try {
-      const result = await fetchUsers(currentPage, limit);
-      setUsers((prevUsers) =>
-        page === 1 ? result : [...prevUsers, ...result]
-      );
-      setIsLastPage(result.length < 6);
-    } catch (error) {
-      setError(error);
-      console.error("Error updating follower count:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   const handleButtonClick = () => {
-    setPage((prevPage) => prevPage + 1);
+    setPage(prevPage => prevPage + 1);
   };
 
   return (
